a943de5d6ad0bb711d83ff99f26dcefbcb6485b6
Tea <teamilas@hotmail.com>
2021-04-09 14:09:08 +0100

    react-create-app on Heroku

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -11,7 +11,24 @@ const Projects = () => {
         <h3 className={styles.projects__title}>PROJECTS</h3>
         <div className={styles.projects}>
             <article className={styles.project}>
-                <img className={styles.project__image} src="https://raw.githubusercontent.com/tea-milas/morse-code-translator/main/assets/screenshot/Screenshot.png" alt="morse code to english translator screenshot"/>
+                <img className={styles.project__image} src="https://raw.githubusercontent.com/tea-milas/notesy-app/main/src/assets/img/Screenshot_login.png"/>
+                <section className={styles.project__description}>
+                    <h4>NOTESY</h4>
+                    <p>Notesy is a note taking app. It was built using React, ES6, SASS and Firebase. It allows users to register, login with email and password or login with a google account, add/delete notes and sign out at the end of their session.</p>
+                    <ul>
+                        <li><FontAwesomeIcon icon={['fab', 'sass']}/></li>
+                        <li><FontAwesomeIcon icon={['fab', 'react']}/></li>
+                        <li><FontAwesomeIcon icon={['fab', 'html5']}/></li>
+                    </ul>
+                    <div className={styles.project__links}>
+                        <button><a href="https://github.com/tea-milas/notesy-app" rel="noreferrer">CODE</a></button>
+                        <button><a href="https://notesy-app-tea.herokuapp.com/" rel="noreferrer">WEBSITE</a></button>
+                    </div>
+                </section>   
+            </article>
+
+
+            <article className={styles.project}>
                 <section className={styles.project__description}>
                     <h4>MORSE CODE / ENGLISH TRANSLATOR</h4>
                     <p>This is a translator of morse code to english and vice versa. It includes text to speech for english and audio reproduction for morse code. It's made with JS and tested with Jest.</p>
@@ -25,25 +42,10 @@ const Projects = () => {
                         <button><a href="https://tea-milas.github.io/morse-code-translator/" target="_blank" rel="noreferrer">WEBSITE</a></button>
                     </div>
                 </section>   
+                <img className={styles.project__image} src="https://raw.githubusercontent.com/tea-milas/morse-code-translator/main/assets/screenshot/Screenshot.png" alt="morse code to english translator screenshot"/>
             </article>
 
-            <article className={styles.project}>
-                <section className={styles.project__description}>
-                    <h4>NOTESY</h4>
-                    <p>Notesy is a note taking app. It was built using React, ES6, SASS and Firebase. It allows users to register, login with email and password or login with a google account, add/delete notes and sign out at the end of their session.</p>
-                    <ul>
-                        <li><FontAwesomeIcon icon={['fab', 'sass']}/></li>
-                        <li><FontAwesomeIcon icon={['fab', 'react']}/></li>
-                        <li><FontAwesomeIcon icon={['fab', 'html5']}/></li>
-                    </ul>
-                    <div className={styles.project__links}>
-                        <button><a href="https://github.com/tea-milas/notesy-app" rel="noreferrer">CODE</a></button>
-                        <button><a>WEBSITE</a></button>
-                    </div>
-                </section>   
-                <img className={styles.project__image} src="https://raw.githubusercontent.com/tea-milas/notesy-app/main/src/assets/img/Screenshot_login.png"/>
-            </article>
-
+            
             <article className={styles.project}>
                 <img className={styles.project__image} src="https://raw.githubusercontent.com/tea-milas/calculator/main/assets/svg/calculator_img.jpg"/>
                 <section className={styles.project__description}>
