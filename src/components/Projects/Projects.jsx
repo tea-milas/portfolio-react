a8c6a29382ca1d128ba7105de7483ac47c06e358
Tea <teamilas@hotmail.com>
2021-04-20 18:48:08 +0100

    added knowaste to projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,11 @@
 import React, {useState,useEffect} from 'react'
 import styles from "./Projects.module.scss"
+import knowaste_img from "../../assets/img/knowaste_screenshot.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab, faSass, faJsSquare,faHtml5,faReact,faCss3} from '@fortawesome/free-brands-svg-icons'
 library.add(fab,faSass,faJsSquare,faHtml5,faReact,faCss3);
+
 
 const Projects = () => {
     const [width, setWidth] = useState(window.innerWidth);
@@ -61,9 +63,27 @@ const Projects = () => {
                {!isMobile && <img className={styles.project__image} src="https://raw.githubusercontent.com/tea-milas/morse-code-translator/main/assets/screenshot/Screenshot.png" alt="morse code to english translator screenshot"/>} 
             </article>
 
-            
             <article className={styles.project}>
-                <img className={styles.project__image} src="https://raw.githubusercontent.com/tea-milas/calculator/main/assets/svg/calculator_img.jpg" alt="calculator img"/>
+                <img className={styles.project__image} src={knowaste_img} alt="knowaste img"/>
+                <section className={styles.project__description}>
+                    <h4>KnoWaste</h4>
+                    <p>A 4 week client project - waste reduction service for students and catering canteens. I was involved in different roles, from being a designer, working on the front-end, 
+                        to being a product owner and integrating part of the front-end with Firebase.
+                    </p>
+                    <ul>
+                        <li><FontAwesomeIcon icon={['fab', 'sass']}/></li>
+                        <li><FontAwesomeIcon icon={['fab', 'react']}/></li>
+                        <li><FontAwesomeIcon icon={['fas', 'database']}/></li>
+                    </ul>
+                    <div className={styles.project__links}>
+                        <button><a href="https://github.com/nology-tech/knowaste" target="_blank" rel="noreferrer">CODE</a></button>
+                        <button><a href="https://knowaste-3c92c.web.app/" target="_blank" rel="noreferrer">WEBSITE</a></button>
+                    </div>
+                </section>   
+            </article>   
+
+            <article className={styles.project}>
+                {isMobile && <img className={styles.project__image} src="https://raw.githubusercontent.com/tea-milas/calculator/main/assets/svg/calculator_img.jpg" alt="calculator img"/>}
                 <section className={styles.project__description}>
                     <h4>CALCULATOR</h4>
                     <p>A calculator that can do simple operations between two numbers and non complex multiple operations. It was made with vanilla JavaScript and was tested with Cypres</p>
@@ -76,7 +96,8 @@ const Projects = () => {
                         <button><a href="https://github.com/tea-milas/calculator" target="_blank" rel="noreferrer">CODE</a></button>
                         <button><a href="https://tea-milas.github.io/calculator/" target="_blank" rel="noreferrer">WEBSITE</a></button>
                     </div>
-                </section>   
+                </section> 
+               {!isMobile && <img className={styles.project__image} src="https://raw.githubusercontent.com/tea-milas/calculator/main/assets/svg/calculator_img.jpg" alt="calculator img"/>  }
             </article>   
 
             <article className={styles.project}>
